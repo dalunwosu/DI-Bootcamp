428cdb230bc288ee49356055b58034fe9e17b20f
Nwosu Chukwudalu <chukwudalunwosu10@gmail.com>
2023-01-22 21:16:04 +0200

    Daily Challenge

---
+++ Week 2/Day 1/Daily Challenge/script.js
@@ -0,0 +1,12 @@
+// Exercise 1
+const fruits = ["Banana", "Apples", "Oranges", "Blueberries"];
+fruits.shift()
+fruits.sort()
+fruits.push("Kiwi")
+fruits.splice(0,1)
+fruits.reverse()
+console.log(fruits)
+
+// Exercise 2
+const moreFruits = ["Banana", ["Apples", ["Oranges"], "Blueberries"]];
+console.log(moreFruits[1][1][0])
