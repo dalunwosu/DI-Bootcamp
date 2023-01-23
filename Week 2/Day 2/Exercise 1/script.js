001ab4640ecc1702d1d6e77f45e225cd15ca3324
Nwosu Chukwudalu <chukwudalunwosu10@gmail.com>
2023-01-23 21:38:02 +0200

    Daily Challenge

--- Week 2/Day 2/Exercise 1/script.js
+++ Week 2/Day 2/Exercise 1/script.js
@@ -28,6 +28,17 @@ const value= userCart["prices"][question]
 console.log(`The price of ${question} is ${value} dollars`)
 
 
+let grade = prompt("Enter your grade:");
+
+if (grade > 90) {
+  console.log("A");
+} else if (grade >= 80 && grade <= 90) {
+  console.log("B");
+} else if (grade >= 70 && grade < 80) {
+  console.log("C");
+} else if (grade < 70) {
+  console.log("D");
+}
 
 
 
