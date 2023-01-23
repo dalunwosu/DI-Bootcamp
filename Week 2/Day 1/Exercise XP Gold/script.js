60036488d8df393458353e7ecf0243b487d91fb3
Nwosu Chukwudalu <chukwudalunwosu10@gmail.com>
2023-01-23 09:53:19 +0200

    Day 1

--- Week 2/Day 1/Exercise XP Gold/script.js
+++ Week 2/Day 1/Exercise XP Gold/script.js
@@ -1,7 +1,9 @@
+// Exercise 1
 let sentence = ["my","favorite","color","is","blue"];
 let joinedSentence = sentence.join(" ");
 console.log(joinedSentence);
 
+// Exercise 2
 let str1 = "mix";
 let str2 = "pod";
 
@@ -11,3 +13,9 @@ let str3 = str1First + str2.slice(2);
 let str4 = str2First + str1.slice(2);
 let newWord= str4 + " "+ str3;
 console.log(newWord)
+
+// Exercise 3 
+const num1= +prompt("Put in the number ");
+const num2= +prompt("Put in the number");
+var sum= num1 + num2;
+alert(sum)
