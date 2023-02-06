4e9f965e8ea524d96ccb1e2b501b5393a9f7ab3e
Nwosu Chukwudalu <chukwudalunwosu10@gmail.com>
2023-02-07 00:24:22 +0200

    Daily Challenge

---
+++ Week 4/Day 2/Daily_Challenge/daily_challenge.py
@@ -0,0 +1,17 @@
+# Exercise 1 
+number = int(input("Give me a number: "))
+range_length = int(input("Give me a range: "))
+number_list = [number]
+multiplier = 1
+while len(number_list) < range_length:
+    multiplier += 1
+    multiple = number * multiplier
+    number_list.append(multiple)
+print(number_list)
+
+# Exercise 2 
+from ordered_set import OrderedSet
+word = input("Enter a word: ")
+word_set = OrderedSet(word)
+new_word = "".join(word_set)
+print("The new word without duplicate consecutive letters: " + new_word)
