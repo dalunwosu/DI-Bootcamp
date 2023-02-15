6ed0f24aa6e6b58817793082c76ce75ab42ce1ad
Nwosu Chukwudalu <chukwudalunwosu10@gmail.com>
2023-02-15 11:41:08 +0200

    Circle

--- Week 5/Day 3/Circle/circle.py
+++ Week 5/Day 3/Circle/circle.py
@@ -7,11 +7,12 @@ class Circle:
     def area(self):
         return round((self.radius**2)*22/7,2)
     def __str__(self):
-        return (f"This is a circle with a radius of {self.radius} and area of {self.area()}")
+        return (f"This is a circle with a radius of {self.radius}, a diameter of {self.diameter()} and area of {self.area()}")
     def __repr__(self):
-        return f"Circle with area of {self.area()}"
+        return f"Circle ({self.radius})"
     def __add__(self,other_circle):
-        return self.area() + other_circle.area()
+        result = Circle(self.radius + other_circle.radius) 
+        return result
     def __gt__(self,other_circle):
         return self.area() > other_circle.area()
     def __lt__(self,other_cirle):
@@ -24,7 +25,7 @@ class Circle:
 circle = Circle(3)
 circle1 = Circle(2)
 circle2 = Circle(5)
-circle3 = Circle(5)
+circle3 = circle1 + circle2
 circles = [circle, circle1, circle2]
 circles.sort()
 print(circles)
@@ -33,3 +34,5 @@ print(circle<circle1)
 print(circle1<circle)
 print(circle2 == circle3)
 print(circle)
+print(circle3.diameter())
+print(circle3)
